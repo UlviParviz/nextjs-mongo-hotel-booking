fa7d944f603f004ad7e6812dc9e76489ece4c27f
UlviParviz <ulvispaethe@gmail.com>
2024-06-27 16:55:41 +0100

    added update room, delete room functionality and  error middleware, handler

--- app/api/admin/rooms/route.ts
+++ app/api/admin/rooms/route.ts
@@ -1,5 +1,5 @@
 import dbConnect from "@/server/config/db.connect";
-import { allRooms, newRoom } from "@/server/controllers/room.controllers";
+import { newRoom } from "@/server/controllers/room.controllers";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
@@ -9,12 +9,7 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 
 dbConnect();
 
-router.get(allRooms);
 router.post(newRoom);
-
-export async function GET(request: NextRequest, ctx: RequestContext) {
-  return router.run(request, ctx);
-}
 
 export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,21 +1,20 @@
 import dbConnect from "@/server/config/db.connect";
-import { allRooms, newRoom } from "@/server/controllers/room.controllers";
+import { getRoomDetails } from "@/server/controllers/room.controllers";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
-interface RequestContext {}
+interface RequestContext {
+  params: {
+    id: string;
+  };
+}
 
 const router = createEdgeRouter<NextRequest, RequestContext>();
 
 dbConnect();
 
-router.get(allRooms);
-router.post(newRoom);
+router.get(getRoomDetails);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
-  return router.run(request, ctx);
-}
-
-export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
