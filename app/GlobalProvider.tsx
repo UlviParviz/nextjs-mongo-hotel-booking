4427bd0f6d58b7477275d4222b117e29ba12ccd3
UlviParviz <ulvispaethe@gmail.com>
2024-06-28 10:22:32 +0100

    added header and footer components

---
+++ app/GlobalProvider.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function GlobalProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ components/layouts/Header.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar sticky-top p-0">
+      <div className="container">
+        <div className="col-6 col-lg-3 p-0">
+          <div className="navbar-brand p-0">
+            <a href="/">
+              <img
+                style={{ cursor: "pointer" }}
+                src="./images/logo_size.jpg"
+                alt="Bookly"
+                width={`110px`}
+                height={`110px`}
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="col-6 col-lg-3 mt-md-0 text-end">
+          <div className="ml-4 dropdown d-line">
+            <button
+              className="btn dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <figure className="avatar avatar-nav">
+                <img
+                  src="/images/default_avatar.jpg"
+                  alt="John Doe"
+                  className="rounded-circle placeholder-glow"
+                />
+              </figure>
+              <span className="placeholder-glow ps-1 fw-bold fs-6">
+                {" "}
+                John Doe
+              </span>
+            </button>
+
+            <div
+              className="dropdown-menu w-100"
+              aria-labelledby="dropdownMenuButton1"
+            >
+              <a href="/admin/dashboard" className="dropdown-item">
+                Dashboard
+              </a>
+              <a href="/bookings/me" className="dropdown-item">
+                My Bookings
+              </a>
+              <a href="/me/update" className="dropdown-item">
+                Profile
+              </a>
+              <a href="/" className="dropdown-item text-danger">
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
