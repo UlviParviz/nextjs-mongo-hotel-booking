8dc6aac9843a7ae5f695aa6aa1fa8ac42c5cedfe
UlviParviz <ulvispaethe@gmail.com>
2024-06-30 10:07:29 +0100

    showed user in header and added logout functionality

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,14 +1,23 @@
 "use client";
+import { log } from "console";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const { data } = useSession();
   const router = useRouter();
 
   const handleLogoClick = (e: any) => {
     e.preventDefault();
     router.push("/");
+  };
+
+  console.log(data?.user);
+
+  const logoutHandler = () => {
+    signOut();
   };
 
   return (
@@ -30,51 +39,72 @@ const Header = () => {
         </div>
 
         <div className="col-6 col-lg-3 mt-md-0 text-end">
-          <div className="ml-4 dropdown d-line">
-            <button
-              className="btn dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <figure className="avatar avatar-nav">
-                <img
-                  src="/images/default_avatar.jpg"
-                  alt="John Doe"
-                  className="rounded-circle placeholder-glow"
-                />
-              </figure>
-              <span className="placeholder-glow ps-1 fw-bold fs-6">
-                {" "}
-                John Doe
-              </span>
-            </button>
+          {data?.user ? (
+            <div className="ml-4 dropdown d-line">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <figure className="avatar avatar-nav">
+                  <img
+                    src={
+                      data?.user?.avatar
+                        ? data?.user?.avatar?.url
+                        : "/images/default_avatar.jpg"
+                    }
+                    alt={`${data?.user?.name}`}
+                    className="rounded-circle placeholder-glow"
+                  />
+                </figure>
+                <span className="placeholder-glow ps-1 fw-bold fs-6 text-capitalize">
+                  {" "}
+                  {data?.user?.name}
+                </span>
+              </button>
 
-            <div
-              className="dropdown-menu w-100"
-              aria-labelledby="dropdownMenuButton1"
-            >
-              <a href="/admin/dashboard" className="dropdown-item">
-                Dashboard
-              </a>
-              <a href="/bookings/me" className="dropdown-item">
-                My Bookings
-              </a>
-              <a href="/me/update" className="dropdown-item">
-                Profile
-              </a>
-              <a href="/" className="dropdown-item text-danger">
-                Logout
-              </a>
+              <div
+                className="dropdown-menu w-100"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <Link href="/admin/dashboard" className="dropdown-item">
+                  Dashboard
+                </Link>
+                <Link href="/bookings/me" className="dropdown-item">
+                  My Bookings
+                </Link>
+                <Link href="/me/update" className="dropdown-item">
+                  Profile
+                </Link>
+                <Link
+                  href="/"
+                  className="dropdown-item text-danger"
+                  onClick={logoutHandler}
+                >
+                  Logout
+                </Link>
+              </div>
             </div>
-          </div>
-          <Link
-            href="/login"
-            className="btn btn-danger px-4 text-white login-header-btn float-right"
-          >
-            Login
-          </Link>
+          ) : (
+            <>
+              {data === undefined && (
+                <div className="placeholder-glow">
+                  <figure className="avatar avatar-nv placeholder bg-secondary"></figure>
+                  <span className="placeholder w-25 bg-secondary ms-2"></span>
+                </div>
+              )}
+              {data === null && (
+                <Link
+                  href="/login"
+                  className="btn btn-danger px-4 text-white login-header-btn float-right"
+                >
+                  Login
+                </Link>
+              )}
+            </>
+          )}
         </div>
       </div>
     </nav>
