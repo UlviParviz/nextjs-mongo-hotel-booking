df90f30cbe4e60355f4f7adc8d5c6353d79542f1
UlviParviz <ulvispaethe@gmail.com>
2024-07-05 21:25:44 +0100

    added password update functionality

--- app/me/update/page.tsx
+++ app/me/update/page.tsx
@@ -1,6 +1,10 @@
 import UpdateProfile from "@/components/user/UpdateProfile";
 import React from "react";
 
+const metadata = {
+  title: 'Update Profile - Bookly',
+}
+
 const UpdateProfilePage = () => {
   return (
     <div>
--- app/me/update_password/page.tsx
+++ app/me/update_password/page.tsx
@@ -1,6 +1,9 @@
 import UpdatePassword from '@/components/user/UpdatePassword'
 import React from 'react'
 
+const metadata = {
+  title: 'Update Password - Bookly',
+}
 const UpdatePasswordPage = () => {
   return (
     <div>
